789557c864c26c18e3b0aa23a5493d398148c1aa
rajx7722 <rajx7722@users.noreply.github.com>
2024-09-05 02:50:18 +0530

    begun writing controllers

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({limit:"16kb",})) //jaise site me upar %20 ya kuch ajeeb likha hota na when we do a simple google search , that %20 is space but encoded through this
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter) 
 
 export default app
---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+//copying of disk storage fn from github readme of multer :https://github.com/expressjs/multer 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
